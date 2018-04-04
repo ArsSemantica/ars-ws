81929095cdc40ade8410cbe7fd3b1a0131ff40c8
wolandscat <thomas.beale@openehr.org>
2018-04-04 11:51:19 +0100

    New home page image.

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/templates/_header_top.php');
 		<div class = "titleContainer">
 			
 			<div class = "titleTop imageContainer">
-				<img src = "/images/title_img2.jpg" class = "titleTopImage" />
+				<img src = "/images/home_image.png" class = "titleTopImage" />
 				<div class="title-text-container"><p class = "title">In order to create IT solutions that <em>radically improve</em> a complex work environment such as a modern hospital or a regional health network, we need an approach that is <em>adaptive</em> with respect to domain semantics, but <em>stable</em> with respect to technology.</p></div>
+	<!--
 				<div class="top-right"><a href="/introduction#ehealth_challenge" class = "title-link">The e-Health<br>Challenge</a></div>
 				<div class="mid-right"><a href="/introduction#platform_paradigm" class = "title-link">The open<br>Platform</a></div>
 				<div class="bottom-right"><a href="/introduction#taming_standards" class = "title-link">Taming<br>Standards</a></div>
+	-->
 				<br class="clear">
 			</div>
 				
